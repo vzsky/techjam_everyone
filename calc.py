521bc8b76d83d3971f7c36af2cb16c1ed413ee29
Touch Sungkawichai <mb99n@Touchs-MacBook-Pro.local>
2019-11-30 15:41:12 +0700

    baseline

---
+++ calc.py
@@ -0,0 +1,3 @@
+import math
+def distance (p1, p2) :
+    return math.sqrt((p1['x'] - p2['x'])*(p1['x'] - p2['x']) + (p1['y'] - p2['y'])*(p1['y'] - p2['y']))
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from flask import Flask, request, jsonify
 from http import HTTPStatus
+from calc import distance
 import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/distance', methods=['POST'])
 def home () :
-    body = request.get_json()
-    return jsonify(status="done", content=body), HTTPStatus.OK
+    r = request.get_json()
+    return jsonify(distance=distance(r['first_pos'], r['second_pos'])), HTTPStatus.OK
 
 @app.errorhandler(404)
 def error (e) :
-    return jsonify(error=HTTPStatus.BAD_REQUEST),HTTPStatus.BAD_REQUEST
+    return jsonify(),HTTPStatus.BAD_REQUEST
 
 if __name__ == "__main__" :
     debug = bool(os.getenv('PRIVATE_DEBUG', ''))
